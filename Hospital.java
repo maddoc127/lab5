bf809f8980c165e4fe9bdcefd781c88f6ab15ecd
Deniz Mirza <denizimmirza@gmail.com>
2025-03-20 13:59:51 +0300

    v 2.1

--- Hospital.java
+++ Hospital.java
@@ -1,4 +1,10 @@
 public class Hospital {
     private String hospitalName;
+    private int numberOfDoctors;
+    private int numberOfPatients;
+    private Doctor[] doctors;
+    private Patient[] patients;
+    private int sizeOfDoctors;
+    private int sizeOfPatients;
 
 }
